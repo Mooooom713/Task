d20d4d96704942127951e301e6bd4a4389762efc
atmoyxic <1332720619amy@gmail.com>
2019-06-14 18:26:00 +0800

    sssss

--- src/component/sizebox/index.js
+++ src/component/sizebox/index.js
@@ -28,7 +28,6 @@ class SizeBox extends Component {
     }
 
     chooseSize = (ev) => {
-        var ev = ev || window.event;
         var target = ev.target || ev.srcElement;
         if (target.nodeName.toLowerCase() === 'li') {
             this.setState({
