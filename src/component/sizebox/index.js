ddefd3179aafbb13fac1ebdb5a47e8cb1a63159f
atmoyxic <1332720619amy@gmail.com>
2019-06-14 18:03:06 +0800

    push

--- src/component/sizebox/index.js
+++ src/component/sizebox/index.js
@@ -1,21 +1,75 @@
 import React, { Component } from 'react';
+import { useDispatch } from 'react-redux';
 import './index.css';
+import { ADD_GOODS, ADD_S_SIZE, ADD_M_SIZE, ADD_L_SIZE } from '../../store/actionType';
 
 class SizeBox extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            size: ''
+        };
+    }
+
     render() {
         return (
             <div className="SizeBox">
                 <p>
-                    SIZE<span className="mark">*</span><span></span>
+                    SIZE<span className="mark">*</span><span className="sizeName">{this.state.size}</span>
                 </p>
-                <ul>
+                <ul onClick={this.chooseSize}>
                     <li>S</li>
                     <li>M</li>
                     <li>L</li>
                 </ul>
-                <button>ADD TO CHART</button>
+                <button onClick={this.addToCart}>ADD TO CART</button>
             </div>
         )
+    }
+
+    chooseSize = (ev) => {
+        var ev = ev || window.event;
+        var target = ev.target || ev.srcElement;
+        if (target.nodeName.toLowerCase() === 'li') {
+            this.setState({
+                size: target.innerHTML
+            })
+        }
+    }
+
+    addToCart = () => {
+        const dispatch = useDispatch();
+        var sText = this.state.size;
+        if (sText) {
+            const goodsArr = localStorage.getItem('goodsArr') ? JSON.parse(localStorage.getItem('goodsArr')) : [];
+            goodsArr.push({
+                name: 'Classic Tee',
+                prize: 75,
+                size: this.state.size
+            })
+            localStorage.setItem('goodsArr', JSON.stringify(goodsArr));
+            let type;
+            switch (this.state.size) {
+                case 'S':
+                    type = ADD_S_SIZE;
+                    break;
+                case 'M':
+                    type = ADD_M_SIZE;
+                    break;
+                case 'L':
+                    type = ADD_L_SIZE;
+                    break;
+            }
+            dispatch({
+                type: ADD_GOODS
+            })
+            dispatch({
+                type
+            })
+            this.setState({
+                size: ''
+            })
+        }
     }
 }
 
