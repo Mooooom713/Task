f8e7a41a5e066a90cf4c160db6fb8f5725721886
i355489 <13699497036@163.com>
2019-06-14 14:14:25 +0800

    add

--- src/App.js
+++ src/App.js
@@ -6,19 +6,28 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button>My Cart({})</button>
       </header>
+      <section>
+        <div>
+          <img src='./asset/classic-tee.jpg' alt='Cloth'/>
+        </div>
+      </section>
+      <section>
+        <h2>Classic Tee</h2>
+        <p>$75.00</p>
+        <p>
+
+        </p>
+        <p>
+          SIZE<span>*</span>
+        </p>
+        <ul>
+          <li>S</li>
+          <li>M</li>
+          <li>L</li>
+        </ul>
+      </section>
     </div>
   );
 }
