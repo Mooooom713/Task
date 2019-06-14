210eae81c3ae40c4b31012d3e29867b50d61ecdb
i355489 <13699497036@163.com>
2019-06-14 18:11:58 +0800

    merge5

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import './App.css';
 import Header from './component/header';
 import ImageBox from './component/imagebox';
-import Description from './component/description/index'
-import SizeBox from './component/sizebox/index'
+import Description from './component/description/index';
+import SizeBox from './component/sizebox/index';
+import CartToast from './component/carttoast';
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -12,6 +13,7 @@ function App() {
     <Provider store={store}>
       <div className="App">
         <Header />
+        <CartToast />
         <div className="sectionWrap">
           <ImageBox />
           <div className="rightSide">
--- src/component/carttoast/index.js
+++ src/component/carttoast/index.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import './style.css';
+
+const aList = (count, size) => {
+  return (<li>
+      <img src={require('../../asset/classic-tee.jpg')} alt='Cloth'/>
+      <div>
+        <p>Classic Tee</p>
+        <p>{count} x <span>$75.00</span></p>
+        <p>Siez: {size}</p>
+      </div>
+    </li>);
+}
+
 
 const CartToast = (props) => {
-  const { isClick } = useSelector(state => state);
-  const list = localStorage.goodsArr;
-  const mapArr = [];
-  list.forEach((item, index) => {
-  });
-  return (<div>
+  const { isClick, sCount, mCount, lCount } = useSelector(state => state);
+  const styleName = isClick ? ' toastActive' : '';
+  const sListContent = aList(sCount, 'S');
+  const mListContent = aList(mCount, 'M');
+  const lListContent = aList(lCount, 'L');
+  return (<div className={`toastWrap${styleName}`}>
     <ul>
       {
-        list.map((item) => {
-          return (<li>
-            <img src={require('../../asset/classic-tee.jpg')} alt='Cloth'/>
-            <div>
-              <p>item.name</p>
-              <p></p>
-              <p></p>
-            </div>
-          </li>);
-        })
+        sListContent
+      }
+      {
+        mListContent
+      }
+      {
+        lListContent
       }
     </ul>
   </div>);
